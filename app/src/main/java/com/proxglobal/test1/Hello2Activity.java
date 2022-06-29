a59df3d33681e84c14bf35496e76fadc26dbe031
rolandonogoldgoal <68616009+rolandonogoldgoal@users.noreply.github.com>
2022-06-29 10:20:12 +0700

    hello2

---
+++ app/src/main/java/com/proxglobal/test1/Hello2Activity.java
@@ -0,0 +1,14 @@
+package com.proxglobal.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hello2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello2);
+    }
+}
